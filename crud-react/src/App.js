581bc98705f3d7352e464c353ba90bd7b7ac7de5
AnjanaTharanga24 <anjana24myprof@gmail.com>
2024-07-04 08:38:27 +0000

    new update

--- crud-react/src/App.js
+++ crud-react/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Button from 'react-bootstrap/Button';
+import { Route,Routes } from 'react-router-dom';
+import Dashboard from './components/dashboard/Dashboard';
+import PostUser from './components/postUser/PostUser';
+import UpdateUser from './components/updateUser/UpdateUser';
+import Nomatch from './components/nomatch/Nomatch';
+import Header from './components/header/Header';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Dashboard/>} />
+        <Route path='/user' element={<PostUser/>} />
+        <Route path='/user/:id' element={<UpdateUser/>} />
+        <Route path='*' element={<Nomatch/>} />
+
+      </Routes>
     </div>
   );
 }
