6cacefac76c8255af659a495d642e312c9e861fe
AnjanaTharanga24 <anjana24myprof@gmail.com>
2024-07-04 11:49:54 +0000

    new update

--- crud-node/src/configuration/dbConfig.js
+++ crud-node/src/configuration/dbConfig.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose');
 
-const connectedToMongoDB = async () =>{
-    try{
-        await mongoose.connect("mongodb://localhost:27017/crud_db");
-        console.log("Connected to MongoDB");
-    }catch(error){
-        console.log(`Error connecting to MongoDB: ${error}`);
-    }
-}
+mongoose.connect("mongodb://localhost:27017/crud_db", {
+});
 
-module.exports = {mongoose, connectedToMongoDB};
+mongoose.connection.on("connected", () => {
+    console.log("Connected to MongoDB");
+});
+
+mongoose.connection.on("error", (err) =>{
+    console.error(`MongoDB connection error: ${err}`);
+})
+
+// const connectedToMongoDB = async () =>{
+//     try{
+//         await mongoose.connect("mongodb://localhost:27017/crud_db");
+//         console.log("Connected to MongoDB");
+//     }catch(error){
+//         console.log(`Error connecting to MongoDB: ${error}`);
+//     }
+// }
+
+module.exports = mongoose;
--- crud-react/src/components/postUser/PostUser.js
+++ crud-react/src/components/postUser/PostUser.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import '../postUser/postUser.css'
+import { Button } from "react-bootstrap";
 
 export default function PostUser() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: ""
+  });
+
+  const handleInputChange= (event) =>{
+    const {name,value}= event.target;
+    setFormData({
+        ...formData,
+        [name]:value,
+    });
+  }
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    console.log("name", formData.name);
+    console.log("email", formData.email);
+    console.log("phone", formData.phone)
+  }
   return (
     <div>
-      <h1> post user</h1>
+      
+
+      <div className="card p-3 form-card">
+      <h1 className="mb-4 text-center"> post user</h1>
+        <Form >
+        <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Label>Name</Form.Label>
+            <Form.Control 
+            type="text"
+            name="name" 
+            placeholder="Enter name" 
+            value={formData.name}
+            onChange={handleInputChange}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control 
+            type="email" 
+            name="email"
+            placeholder="Enter email"
+            value={formData.email}
+            onChange={handleInputChange}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formGroupPassword">
+            <Form.Label>Phone</Form.Label>
+            <Form.Control
+             type="number" 
+             name="phone"
+             placeholder="Enter mobile"
+             value={formData.phone}
+             onChange={handleInputChange}
+             />
+          </Form.Group>
+
+          <Button variant="primary" onClick={handleSubmit}>Submit</Button>
+
+
+        </Form>
+      </div>
     </div>
-  )
+  );
 }
