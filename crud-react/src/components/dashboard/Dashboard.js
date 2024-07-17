97662d7f86f9320c7a1ad134d1e7269c60548915
AnjanaTharanga24 <anjana24myprof@gmail.com>
2024-07-17 05:47:51 +0000

    new update on delete user by userId method

--- crud-react/src/components/dashboard/Dashboard.js
+++ crud-react/src/components/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useId, useState } from 'react'
 import Table from 'react-bootstrap/Table';
 import '../dashboard/dashboard.css';
 import Button from 'react-bootstrap/Button';
@@ -8,22 +8,37 @@ export default function Dashboard() {
   const [users,setUsers]= useState([]);
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    const fetchUsers = async()=>{
-      try {
-        const response =await fetch("http://localhost:5000/api/user");
-        const data = await response.json();
-        setUsers(data);
-        console.log(data);
-      } catch (error) {
-        console.error("error while fetching users:",error.message);
-      }
+  const fetchUsers = async()=>{
+    try {
+      const response =await fetch("http://localhost:5000/api/user");
+      const data = await response.json();
+      setUsers(data);
+      console.log(data);
+    } catch (error) {
+      console.error("error while fetching users:",error.message);
     }
+  }
+
+  useEffect(()=>{
     fetchUsers();
   },[]);
 
   const handleUpdate = (userId) => {
-      navigate(`/user/${userId}`)
+      navigate(`/user/${userId}`);
+  }
+
+  const handleDelete = async (userId) => {
+    try {
+      const response = await fetch(`http://localhost:5000/api/user/${userId}`, {
+        method: "DELETE"
+      });
+      console.log(response);
+      if (response.ok) {
+        fetchUsers();
+      }
+    } catch (error) {
+      console.error("Error while deleting user:", error.message);
+    }
   }
   return (
     <div>
@@ -45,7 +60,7 @@ export default function Dashboard() {
            <td>{user.phone}</td>
            <td>
            <Button variant="primary" onClick={() => handleUpdate(user._id)}>Update</Button>{' '}
-           {/* <Button variant="danger" onClick={()=> handleDelete(user._id)}>Delete</Button>{' '} */}
+           <Button variant="danger" onClick={() => handleDelete(user._id)}>Delete</Button>{' '}
 
            </td>
          </tr>
